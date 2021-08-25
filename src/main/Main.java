77bc4a6325b65d50cd6bac0340b228f638422f96
Meng <dreamsjtu@gmail.com>
2021-08-25 19:43:23 +1200

    use two threads

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
 import control.Controller;
 import model.Board;
 import view.SnakeGameGUI;
 
 public class Main {
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+		@Override
+		public void run() {
 		//create the game board, 20 rows, 20 cols, 3 is the length of the snake body.
 		Board board = new Board(20, 20, 3);
 		//create the controller of the game
@@ -13,6 +18,10 @@ public class Main {
 		//create the GUI for the game.
 		new SnakeGameGUI(board,control);
 		//run the game.
-		control.run();
+		Thread thread =  new  Thread(control);
+        thread.start();
 	}
+	});
+		
+}
 }
